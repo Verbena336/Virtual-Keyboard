77992f4bb244e220797f01963340a60d0a84e40d
Natalia Инящмф <nata22011997@gmail.com>
2022-05-09 14:21:29 +0300

    init: deploy of project Virtual-Keyboard

---
+++ js/helpers/button.js
@@ -0,0 +1,27 @@
+import createElem from './createElem.js';
+
+export default class Button {
+  constructor({ letter, shift, key }) {
+    this.letter = letter;
+    this.shift = shift;
+    this.key = key;
+    this.classesArr = ['keyboard__key'];
+    if (this.key === 'Backspace' || this.key === 'Enter') {
+      this.classesArr.push('keyboard__key--size-l');
+    } else if (this.key === 'Tab' || this.key === 'Delete' || this.key === 'ControlLeft' || this.key === 'ControlRight') {
+      this.classesArr.push('keyboard__key--size-xs');
+    } else if (this.key === 'CapsLock') {
+      this.classesArr.push('keyboard__key--size-s');
+      this.light = createElem('div', ['light']);
+    } else if (this.key === 'ShiftLeft' || this.key === 'ShiftRight') {
+      this.classesArr.push('keyboard__key--size-m');
+    } else if (this.key === 'Space') {
+      this.classesArr.push('keyboard__key--size-xl');
+    }
+    this.button = createElem('div', this.classesArr, letter);
+    this.button.dataset.key = `${this.key}`;
+    if (this.light) {
+      this.button.append(this.light);
+    }
+  }
+}
